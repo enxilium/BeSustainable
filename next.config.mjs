f492591e62ce7907fd51091cecc0545770e89745
PikaKight <tmmarcus73@gmail.com>
2024-10-06 03:46:17 -0400

    fixing

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,6 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -11,5 +14,9 @@ const nextConfig = {
       ],
     },
   };
-  
+
+  if (process.env.NODE_ENV === 'development') {
+    await setupDevPlatform();
+  }
+
   export default nextConfig;
